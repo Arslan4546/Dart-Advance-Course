9ec28db51a24f61124748e89b348342320a8b50f
Arslan Tariq <arslantariq4546@gmail.com>
2024-08-20 15:51:06 +0500

    add null safty concept

---
+++ Dart Advance Concepts/Null Safty/prac1.dart
@@ -0,0 +1,24 @@
+// void main(List<String> args) {
+//   int? x; //null-able variale
+//   print(x);
+// }
+
+//output:null
+
+// void main(List<String> args) {
+//   int? x; //null-able variale
+
+//   int y = x; // give error
+//   print(x);
+// }
+
+//output:error
+
+// void main(List<String> args) {
+//   int? x; //null-able variale
+
+//   int y = x!; // not give an error
+//   print(y);
+// }
+
+//output : Unhandeled Exception
---
+++ Dart Advance Concepts/Null Safty/prac2.dart
@@ -0,0 +1,18 @@
+// void main(List<String> args) {
+//   int? age; // null-able variable
+//   String? name; // null-able variable
+
+//   print(age);
+
+//   name = "Arslan"; // non-nullable variable
+//   print(name);
+// }
+
+class Car {
+  String carName = "Aston Martin";
+}
+
+void main() {
+  Car cars;
+  print(cars.carName);
+}
