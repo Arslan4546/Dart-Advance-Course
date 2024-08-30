85e1004804908fcaf515049e337a956873647205
Arslan Tariq <arslantariq4546@gmail.com>
2024-08-30 23:57:48 +0500

    add function backend concepts

---
+++ Dart Advance Concepts/Functions/FunBackgroundConcepts/FunConcepts.dart
@@ -0,0 +1,22 @@
+// void main(List<String> args) {
+//   Function f = Sum;
+//   print(f(2, 3));
+// }
+
+// num Sum(num a, num b) {
+//   return a + b;
+// }
+
+// ye ik simple function hai or jab he humm koi function banaty hain compiler uss ki implicity backend py ik just callAble class bna deta  hai or aisy he jab bhi koi function banyn gy hum har bari compiler ik callable class bna dy ga implicily
+
+// or jo compiler class create krta hai jo Backend per bani hai wo class Function Class ko extends kry gee mtlab ab Sum function ab function class ki properties bhi access kr skti hai
+
+void main() {
+  Student s = Student();
+}
+
+class Student {
+  void sum(String name) {
+    print(name);
+  }
+}
