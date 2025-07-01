cd485f73cada1c844377d0a0bb5f9d8bd4ea6005
Arslan Tariq <arslantariq4546@gmail.com>
2025-07-01 00:57:59 -0700

    update

--- Dart Advance Concepts/AsyncProgramming/MoreAboutStream/Academy.dart
+++ Dart Advance Concepts/AsyncProgramming/MoreAboutStream/Academy.dart
@@ -21,19 +21,19 @@ void main(List<String> args) async {
 //   }
 // }
 
-
 Stream<int> generateNumber(int max) {
   StreamController<int> streamController = StreamController<int>();
   StreamSink<int> streamSink = streamController.sink;
   for (var i = 0; i < max; i++) {
     streamController.sink.add(
-        i); // ye phla tareqa hai bagair kissi streamsink create app controller sy add kr skty hain stream mai
+      i,
+    ); // ye phla tareqa hai bagair kissi streamsink create app controller sy add kr skty hain stream mai
 
-    streamSink.add(
-        i); // ye dosra tareq hai without controller k but apko ko iss k lyee controller ko create krna pry ga
+    // streamSink.add(
+    //   i,
+    // ); // ye dosra tareq hai without controller k but apko ko iss k lyee controller ko create krna pry ga
   }
   return streamController.stream;
 }
+
 // ye function generative ni hai but ye controller k through stream ko sink krti hai or ye 2 method sy sink hoee hai
-
-
